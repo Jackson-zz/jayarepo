84603a2f57eaef98b7c346e47fe4aa29b22dae47
Eliot Horowitz <eliot@10gen.com>
2008-07-07 21:56:39 -0400

    cleaning - only use 1 controller

--- _init.js
+++ _init.js
@@ -6,4 +6,8 @@ local.models.utils();
 local.models.course();
 local.models.student();
 
+core.core.routes();
 
+routes = new Routes();
+routes.student = "/student.jxp";
+routes.add( "students" , "/student.jxp" , { extra : { action : "list" } } );
